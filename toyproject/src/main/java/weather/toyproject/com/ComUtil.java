f1c74617682c4cd1e472dd85dac03642188490ac
awesomejang <uljang1995@gmail.com>
2021-10-11 23:45:51 +0900

    격자 <-> GPS변환 클래스 수정

--- toyproject/src/main/java/weather/toyproject/com/ComUtil.java
+++ toyproject/src/main/java/weather/toyproject/com/ComUtil.java
@@ -2,16 +2,4 @@ package weather.toyproject.com;
 
 public class ComUtil {
 	
-	public static boolean IsNumber(String s) {
-		if(s == null || s.equals("")) {
-			return false;
-		}
-		
-		try {
-			Integer.parseInt(s);
-			return true;
-		} catch (NumberFormatException e) {
-			return false;
-		}
-	}
 }
