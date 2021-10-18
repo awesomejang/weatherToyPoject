37b7f3ea8b9e393ebf4706732983771ac02fb042
awesomejang <uljang1995@gmail.com>
2021-10-18 23:53:21 +0900

    index.html 수정 및 css, js 파일추가

---
+++ toyproject/src/main/java/weather/toyproject/controller/UserController.java
@@ -0,0 +1,13 @@
+package weather.toyproject.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class UserController {
+	
+	@GetMapping("/weblogin/create_account")
+	public String createAccount() {
+		return "/user/join/user_join";
+	}
+}
