a5406d59fa577054d7a385dd0be275f39ac81161
awesomejang <uljang1995@gmail.com>
2021-10-29 23:42:51 +0900

    스프링 시큐리티 작업중

--- toyproject/src/main/java/weather/toyproject/WebSecurityConfig.java
+++ toyproject/src/main/java/weather/toyproject/WebSecurityConfig.java
@@ -1,6 +1,7 @@
 package weather.toyproject;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -12,8 +13,9 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
+	//== 인증을 무시할 경로들을 설정 ==// 
 	public void configure(WebSecurity web) {
-		// 인증을 무시할 경로들을 설정 -> static 하위폴더 설정 
+		//static 하위폴더 설정
 		web.ignoring()
 		   .antMatchers("/css/**", "/script/**", "/img/**")
 		   .antMatchers("/resources/**");
@@ -27,8 +29,21 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 			.antMatchers("/index.html", "/login", "/signup", "/user").permitAll() // 누구나 접근가능 
 			.antMatchers("/").hasAnyRole("USER") // ADMIN의 auth는 ROLE_ADMIN, ROLE_USER의 형태 
 			.antMatchers("/admin").hasRole("ADMIN")
-			.anyRequest().authenticated(); // 위의 설정 외 요청들은 권한의 종류에 상관 없이 권한이 있어야 접근 
-		
-		
+			//.anyRequest().authenticated(); // 위의 설정 외 요청들은 권한의 종류에 상관 없이 권한이 있어야 접근 
+		.and()
+		 	.formLogin() // 로그인 관련 설정 시작
+		 	 .loginPage("/login") // 로그인 페이지 URL
+		 	 .defaultSuccessUrl("/") // 로그인 성공시 default redirect url 
+		.and()
+			.logout() // 로그아웃 관련 설정 시
+			 .logoutSuccessUrl("/login") // 로그아웃 성공시 default redirect url
+			 .invalidateHttpSession(true) // 기존 세션날리기
+	    ;
+	}
+	
+	@Override
+	//== 로그인 로직 처리==//
+	public void configure(AuthenticationManagerBuilder auth) throws Exception {
+		//auth.userDetailsService(null)
 	}
 }
--- toyproject/src/main/java/weather/toyproject/controller/user/domain/CustomUserDetails.java
+++ toyproject/src/main/java/weather/toyproject/controller/user/domain/CustomUserDetails.java
@@ -1,5 +1,65 @@
 package weather.toyproject.controller.user.domain;
 
-public class CustomUserDetails {
+import java.util.Collection;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import lombok.Getter;
+
+@Getter
+public class CustomUserDetails implements UserDetails{
+	
+	private String id;
+	
+	private String password;
+	
+	private String auth;
+	
+	private String email;
+	
+	//private String 
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getPassword() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getUsername() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		// TODO Auto-generated method stub
+		return false;
+	}
 
 }
