6053e746ba0d07ffd3ed3190598a5d0250ae5334
awesomejang <uljang1995@gmail.com>
2021-10-31 23:55:59 +0900

    스프링 시큐리티 작업중

--- toyproject/src/main/java/weather/toyproject/controller/user/domain/CustomUserDetails.java
+++ toyproject/src/main/java/weather/toyproject/controller/user/domain/CustomUserDetails.java
@@ -4,6 +4,7 @@ import java.util.Collection;
 
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.User;
 
 import lombok.Getter;
 
@@ -18,48 +19,76 @@ public class CustomUserDetails implements UserDetails{
 	
 	private String email;
 	
-	//private String 
+	private boolean emailVerified;
+	
+	private boolean locked;
+	
+	private String nickname;
+	
+	private Collection<GrantedAuthority> authorities; // 권한 목록
 
+	/**
+	 * 해당 사용자의 권한 목록
+	 */
 	@Override
 	public Collection<? extends GrantedAuthority> getAuthorities() {
-		// TODO Auto-generated method stub
-		return null;
+		return authorities;
 	}
 
+	/**
+	 * 비밀번호
+	 */
 	@Override
 	public String getPassword() {
-		// TODO Auto-generated method stub
-		return null;
+		return password;
 	}
 
+	/**
+	 * PK값 username : 사용자의 고유값 
+	 */
 	@Override
 	public String getUsername() {
-		// TODO Auto-generated method stub
-		return null;
+		return id;
 	}
 
+	/**
+	 * 계정 만료 여부
+	 * true : 만료 안됨
+	 * false : 만료
+	 * 계정 만료를 확인할 필요가 없다면 true 고정 리턴 
+	 */
 	@Override
 	public boolean isAccountNonExpired() {
-		// TODO Auto-generated method stub
-		return false;
+		return true;
 	}
 
+	/**
+	 * true : 잠기지 않음
+	 * false : 잠김
+	 */
 	@Override
 	public boolean isAccountNonLocked() {
-		// TODO Auto-generated method stub
-		return false;
+		return locked;
 	}
 
+	/**
+	 * 비밀번호 만료 여부
+	 * true : 만료 안됨
+	 * false : 만료
+	 */
 	@Override
 	public boolean isCredentialsNonExpired() {
-		// TODO Auto-generated method stub
-		return false;
+		return true;
 	}
 
+	/**
+	 * 사용자 활성화 여부
+	 * true : 활성화
+	 * false : 비활성화
+	 */
 	@Override
 	public boolean isEnabled() {
-		// TODO Auto-generated method stub
-		return false;
+		return true;
 	}
 
 }
