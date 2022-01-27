f823dbf264a56b88927426b2bbcfa909c4189865
awesomejang <uljang1995@gmail.com>
2022-01-27 23:55:02 +0900

    회원가입 로직 작성중

--- toyproject/src/main/java/weather/toyproject/haru/user/UserController.java
+++ toyproject/src/main/java/weather/toyproject/haru/user/UserController.java
@@ -25,6 +25,7 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -61,26 +62,33 @@ public class UserController {
 	}
 	
 	@PostMapping("/user/new")
-	public String UserRegistProcess(@ModelAttribute("userVO") @Valid UserVO userVO, Errors errors, HttpServletRequest request
-			, HttpServletResponse response, Model model, RedirectAttributes redirectAttributes) {
+	public String UserRegistProcess(@ModelAttribute @Valid UserVO userVO, Errors errors
+			                       ,HttpServletRequest request, @RequestParam("userIdDupCheck") String userIdDupResult
+			                       , HttpServletResponse response, Model model, RedirectAttributes redirectAttributes) {
 		
 		//@valid : 클라이언트의 입력 데이터가 dto클래스로 캡슐화되어 넘어올 때, 유효성을 체크하라는 어노테이션
 		//Errors : vo에 binding된 필드의 유효성 검사 오류에 대한 정보를 저장하고 노출합니다.
 		Map<String, String> validatorResult = userService.UserValidateHandling(userVO, errors);
+		redirectAttributes.addFlashAttribute("userVO", userVO);
+		
 		
 		if(validatorResult.isEmpty() != true) {
 			userService.addValidAttribute(validatorResult, redirectAttributes);
-			
-			redirectAttributes.addFlashAttribute("userVO", userVO);
+			//redirectAttributes.addFlashAttribute("userVO", userVO);
+			return "redirect:/user/new";
+		}
+		
+		if(userIdDupResult.equals("N")) {
+			redirectAttributes.addFlashAttribute("userRegistMsg", "아이디 중복확인을 해주세요."); //URL
 			return "redirect:/user/new";
 		}
 		
 		boolean result = userService.InsertUser(userVO);
+		
 		if(result != true) {
 			log.info("userRegist_fail = {}", userService.InsertUser(userVO));
-			 
 			redirectAttributes.addFlashAttribute("userRegistMsg", environment.getProperty("user.regist.fail.msg"));
-			redirectAttributes.addFlashAttribute("userVO", userVO);
+			//redirectAttributes.addFlashAttribute("userVO", userVO);
 			return "redirect:/user/new";
 		}
 		
@@ -88,8 +96,8 @@ public class UserController {
 		return "redirect:/";
 	}
 	
-	@GetMapping("/user/DupCheck/{userId}")
 	@ResponseBody
+	@GetMapping(value = {"/user/DupCheck/{userId}", "/user/DupCheck/"}) //{userId} 가 null일때 404발생 -- 2개를 잡아서 처리
 	public Map<String, String> userIdDupCheck(@PathVariable(required = false) String userId) {
 		Map<String, String> result = userService.userIdDupCheck(userId);
 		return result;
