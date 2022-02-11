3618328a00b0b43b1c8967f6e3df8eb1db1fd193
awesomejang <uljang1995@gmail.com>
2022-02-11 23:45:23 +0900

    게임 게시판 작성중

--- toyproject/src/main/java/weather/toyproject/haru/border/BorderController.java
+++ toyproject/src/main/java/weather/toyproject/haru/border/BorderController.java
@@ -1,14 +1,18 @@
 package weather.toyproject.haru.border;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @Controller
 public class BorderController {
-
-	public String gameBorderList() {
-		return "Test";
+	
+	@GetMapping(value = {"/game/{gameCode}", "/game", "/game/"})
+	public String gameBorderList(@PathVariable(required = false) Long gameCode) {
+		log.info("gameCode ={}", gameCode);
+		return "board/gameBoard";
 	}
 }
