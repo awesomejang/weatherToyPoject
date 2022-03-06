d47d958b5c2e25529f9623c877aad914b3f0571a
awesomejang <uljang1995@gmail.com>
2022-03-07 01:54:11 +0900

    관리자 게임등록페이지 작성중

--- toyproject/src/main/java/weather/toyproject/haru/game/GameController.java
+++ toyproject/src/main/java/weather/toyproject/haru/game/GameController.java
@@ -1,6 +1,7 @@
 package weather.toyproject.haru.game;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -8,5 +9,8 @@ import lombok.extern.slf4j.Slf4j;
 @Controller
 public class GameController {
 
-	//public 
+	@GetMapping("/admin/game/regist")
+	public String gameRegist() { 
+		return "game/gameRegist";
+	}
 }
